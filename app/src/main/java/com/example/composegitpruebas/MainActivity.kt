eeeefe9935da277c1d1034497d62fb44ba40be8f
igor <igor.baquero@gmail.com>
2025-03-20 18:17:33 +0000

    agregado boton y centrado

--- app/src/main/java/com/example/composegitpruebas/MainActivity.kt
+++ app/src/main/java/com/example/composegitpruebas/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composegitpruebas.ui.theme.ComposeGitPruebasTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeGitPruebasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Greeting(name = "Android")
+                    }
                 }
             }
         }
@@ -32,10 +41,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Button(onClick = { /* Acción del botón */ }, modifier = modifier.wrapContentSize()) {
+        Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
